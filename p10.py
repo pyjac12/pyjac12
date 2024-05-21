22257a08ceab0a5c26f73f7b5f773a59a28964ff
pyjac12 <mansionjoker24@gmail.com>
2024-05-21 10:42:21 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,7 @@
+name=input("what is your name?")
+quest=input("what is our quest?")
+color=input("what is your fav color?")
+print("your name is " +   name + '.')
+print("our quest is "  +  quest + ".")
+print("my fav color is " +  color + ".")
+print(type(name))
